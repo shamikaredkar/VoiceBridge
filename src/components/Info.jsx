73fcca592a85192b67de637983f9e18d6c570d79
Shamika Redkar <shamikaredkar91@gmail.com>
2024-08-27 15:47:35 -0400

    Added copy and download buttons

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -5,6 +5,13 @@ import Translation from "./Translation";
 export default function Info(props) {
   const { output } = props;
   const [tab, setTab] = useState("transcription");
+  function handleCopy() {
+    navigator.clipboard.writeText();
+  }
+  function handleDownload() {
+    const element = document.createElement("a");
+    const file = new Blob([]);
+  }
   return (
     <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 justify-center text-center text-center pb-20 max-w-prose w-full mx-auto'>
       <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl whitespace-nowrap'>
@@ -38,11 +45,27 @@ export default function Info(props) {
           Translation
         </button>
       </div>
-      {tab === "transcription" ? (
-        <Transcription {...props} />
-      ) : (
-        <Translation {...props} />
-      )}
+      <div className='my-8 flex flex-col'>
+        {tab === "transcription" ? (
+          <Transcription {...props} />
+        ) : (
+          <Translation {...props} />
+        )}
+      </div>
+      <div className='flex items-center gap-4 mx-auto'>
+        <button
+          title='Copy'
+          className='bg-white text-blue-300 px-2 rounded aspect-square grid place-items-center hover:text-blue-500 duration-200'
+        >
+          <i className='fa-solid fa-copy'></i>
+        </button>
+        <button
+          title='Download'
+          className='bg-white text-blue-300 px-2 rounded aspect-square grid place-items-center hover:text-blue-500 duration-200'
+        >
+          <i className='fa-solid fa-download'></i>
+        </button>
+      </div>
     </main>
   );
 }
