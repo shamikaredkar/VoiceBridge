e7b1d7cc3cd40c9902c56b5c6911d37e32587c3b
Shamika Redkar <shamikaredkar91@gmail.com>
2024-08-27 16:22:23 -0400

    Created basic UI for Translation

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -7,7 +7,7 @@ export default function Info(props) {
   const [tab, setTab] = useState("transcription");
   const [translation, setTranslation] = useState(null);
   const [translating, setTranslating] = useState(null);
-  const [toLanguage, setToLanguage] = useState(null);
+  const [toLanguage, setToLanguage] = useState("Select Language");
   function handleCopy() {
     navigator.clipboard.writeText();
   }
@@ -21,12 +21,15 @@ export default function Info(props) {
   }
 
   function generateTranslatiin() {}
-  const textElement = tab === 'transcription' ? output.map(val => val.text) : 
+  const textElement =
+    tab === "transcription" ? output.map((val) => val.text) : "";
 
   return (
     <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 justify-center text-center text-center pb-20 max-w-prose w-full mx-auto'>
       <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl whitespace-nowrap'>
-        <span className='text-blue-400 bold'>Transcription: </span>
+        <span className='text-blue-400 bold'>
+          {tab === "transcription" ? "Transcription" : "Translation"}
+        </span>
       </h1>
       <div className='grid grid-cols-2 items-center bg-white shadow rounded-full overflow-hidden mx-auto'>
         <button
@@ -60,7 +63,15 @@ export default function Info(props) {
         {tab === "transcription" ? (
           <Transcription {...props} textElement={textElement} />
         ) : (
-          <Translation {...props} />
+          <Translation
+            {...props}
+            toLanguage={toLanguage}
+            translating={translating}
+            translation={translation}
+            setTranslating={setTranslating}
+            setTranslation={setTranslation}
+            setToLanguage={setToLanguage}
+          />
         )}
       </div>
       <div className='flex items-center gap-4 mx-auto'>
--- src/components/Transcribing.jsx
+++ src/components/Transcribing.jsx
@@ -5,7 +5,7 @@ export default function Transcribing({ downloading }) {
     <div className='flex items-center flex-col justify-center gap-10 md:gap-14 pb-24 p-4 text-center flex-1'>
       <div className='flex flex-col gap-2 sm:gap-4'>
         <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl'>
-          <span className='text-blue-400 bold'>Transcribing!</span>
+          <span className='text-blue-400 bold'>Transcribing...</span>
         </h1>
         <p>
           {!downloading ? "Warming up cylinders" : "Core cylinders Engaged"}
